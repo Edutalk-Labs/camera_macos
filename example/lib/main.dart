c15b7d20363934043dee34b56a1e95dba3df47d8
Binh Do <binhdothanh.navn@gmail.com>
2024-12-06 11:39:17 +0700

    add sources

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
-  final _cameraMacosPlugin = CameraMacos();
+  final _cameraMacosPlugin = MacOSCamera();
 
   @override
   void initState() {
@@ -30,21 +30,14 @@ class _MyAppState extends State<MyApp> {
     String platformVersion;
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _cameraMacosPlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
+    try {} on PlatformException {}
 
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
     if (!mounted) return;
 
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+    setState(() {});
   }
 
   @override
